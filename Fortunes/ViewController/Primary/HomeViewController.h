cf1532e94f33dfbc0bc4b6ea08d53f7917312f12
he1ko <h-bublitz@gmx.de>
2015-07-31 11:50:03 +0200

    doku

--- Fortunes/ViewController/Primary/HomeViewController.h
+++ Fortunes/ViewController/Primary/HomeViewController.h
@@ -1,12 +1,8 @@
-//
-//  HomeViewController.h
-//  Fortunes
-//
-//  Created by Heiko Bublitz on 20.07.15.
-//  Copyright (c) 2015 Jemix. All rights reserved.
-//
-
 #import "BaseViewController.h"
+
+/*!
+  @discussion Main display of a random Fortune in full screen view
+ */
 
 @interface HomeViewController : BaseViewController
 
